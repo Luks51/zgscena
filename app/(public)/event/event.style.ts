7f66162a1cd78c924c3ec316725cd436ff6ed6c2
Mihael Smrcek <25547789+khromac@users.noreply.github.com>
2024-11-23 13:35:57 +0100

    added static event page

---
+++ app/(public)/event/event.style.ts
@@ -0,0 +1,88 @@
+import { StyleSheet } from "react-native";
+import {Colors} from "@/constants/Colors";
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+    padding: 16,
+  },
+  image: {
+    width: "100%",
+    height: 200,
+    marginBottom: 16,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 16,
+  },
+  detailsContainer: {
+    marginBottom: 16,
+  },
+  iconRow: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 12,
+  },
+  placeholderIcon: {
+    width: 40,
+    height: 40,
+    backgroundColor: "#ccc", // Placeholder color for the icon
+    borderRadius: 20,
+    marginRight: 8,
+  },
+  detailText: {
+    fontSize: 16,
+    color: Colors.light.text,
+  },
+  description: {
+    fontSize: 14,
+    color: Colors.light.text,
+    lineHeight: 20,
+    marginBottom: 16,
+  },
+  footerContainer: {
+    // borderWidth: 1,
+    gap: 3,
+    display: "flex",
+    flexDirection: "row",
+    marginTop: "auto",
+    alignItems: "center",
+    justifyContent: "space-evenly",
+  },
+  footerButton: {
+    backgroundColor: Colors.light.tint,
+    // borderWidth: 1,
+    // borderColor: Colors.light.tint,
+    display: "flex",
+    flexDirection: "row",
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 8,
+  },
+  footerButton2: {
+    // backgroundColor: Colors.light.tint,
+    borderWidth: 1,
+    borderColor: Colors.light.tint,
+    paddingVertical: 12,
+    paddingHorizontal: 12,
+    borderRadius: 8,
+  },
+  footerButtonText: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  iconBox: {
+    width: 40,
+    height: 40,
+    backgroundColor: "#f3f3f3",
+    borderRadius: 5,
+    marginRight: 8,
+  },
+  iconCenter: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
--- app/(tabs)/home/components/eventcard.tsx
+++ app/(tabs)/home/components/eventcard.tsx
@@ -1,3 +1,4 @@
+import { Platform } from "react-native";
 import globalStyles from "@/app/style";
 import { ThemedText } from "@/components/ThemedText";
 import { IconSymbol } from "@/components/ui/IconSymbol";
@@ -10,53 +11,54 @@ export default function EventCardComponent({
   eventValue,
 }: {
   eventValue: EventModel;
+    routeId: number;
 }) {
   const colorScheme = useColorScheme();
   return (
-    <View style={{ padding: 5 }}>
-      <View
-        style={[
-          globalStyles.boxShadow,
-          { backgroundColor: Colors[colorScheme ?? "light"].background },
-          globalStyles.p2,
-          styles.eventCard,
-        ]}
-      >
-        <Image src={eventValue.src} />
-        <ThemedText>{eventValue.title}</ThemedText>
-        <View style={[globalStyles.dFlex, globalStyles.alignItemsCenter]}>
-          <IconSymbol
-            size={28}
-            name="location.pin"
-            color={Colors[colorScheme ?? "light"].text}
-          />
-          <ThemedText>{eventValue.location}</ThemedText>
+        <View style={{ padding: 5 }}>
+          <View
+            style={[
+              globalStyles.boxShadow,
+              { backgroundColor: Colors[colorScheme ?? "light"].background },
+              globalStyles.p2,
+              styles.eventCard,
+            ]}
+          >
+            <Image src={eventValue.src} />
+            <ThemedText>{eventValue.title}</ThemedText>
+            <View style={[globalStyles.dFlex, globalStyles.alignItemsCenter]}>
+              <IconSymbol
+                size={28}
+                name="location.pin"
+                color={Colors[colorScheme ?? "light"].text}
+              />
+              <ThemedText>{eventValue.location}</ThemedText>
+            </View>
+            <View style={[globalStyles.dFlex, globalStyles.alignItemsCenter]}>
+              <IconSymbol
+                size={28}
+                name="calendar.month"
+                color={Colors[colorScheme ?? "light"].text}
+              />
+              <ThemedText>{String(eventValue.date)}</ThemedText>
+            </View>
+            <View style={[globalStyles.dFlex, globalStyles.alignItemsCenter]}>
+              <IconSymbol
+                size={28}
+                name="euro"
+                color={Colors[colorScheme ?? "light"].text}
+              />
+              <ThemedText>{eventValue.price}</ThemedText>
+            </View>
+            <View style={[globalStyles.dFlex, globalStyles.alignItemsCenter]}>
+              <IconSymbol
+                size={28}
+                name="local.fire.department"
+                color={Colors[colorScheme ?? "light"].text}
+              />
+              <ThemedText>{eventValue.numberOfPeoples}</ThemedText>
+            </View>
+          </View>
         </View>
-        <View style={[globalStyles.dFlex, globalStyles.alignItemsCenter]}>
-          <IconSymbol
-            size={28}
-            name="calendar.month"
-            color={Colors[colorScheme ?? "light"].text}
-          />
-          <ThemedText>{String(eventValue.date)}</ThemedText>
-        </View>
-        <View style={[globalStyles.dFlex, globalStyles.alignItemsCenter]}>
-          <IconSymbol
-            size={28}
-            name="euro"
-            color={Colors[colorScheme ?? "light"].text}
-          />
-          <ThemedText>{eventValue.price}</ThemedText>
-        </View>
-        <View style={[globalStyles.dFlex, globalStyles.alignItemsCenter]}>
-          <IconSymbol
-            size={28}
-            name="local.fire.department"
-            color={Colors[colorScheme ?? "light"].text}
-          />
-          <ThemedText>{eventValue.numberOfPeoples}</ThemedText>
-        </View>
-      </View>
-    </View>
   );
 }

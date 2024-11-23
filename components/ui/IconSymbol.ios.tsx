53a642883da5bc187cd8603c51572e5adbcd3389
Luka Vukovic <luka.vukovic511@gmail.com>
2024-11-23 17:00:57 +0100

    id:main

--- components/ui/IconSymbol.ios.tsx
+++ components/ui/IconSymbol.ios.tsx
@@ -7,29 +7,34 @@ import { OpaqueColorValue, StyleProp, ViewStyle } from "react-native";
 
 // Add your SFSymbol to MaterialIcons mappings here.
 const MAPPING = {
-    // See MaterialIcons here: https://icons.expo.fyi
-    // See SF Symbols in the SF Symbols app on Mac.
-    "house.fill": "home",
-    "paperplane.fill": "send",
-    "chevron.left.forwardslash.chevron.right": "code",
-    "chevron.right": "chevron-right",
-    "notifications.none": "notifications-none",
-    "calendar.today": "calendar-today",
-    "people": "emoji-people",
-    "location": "location-pin",
-    "bookmark": "bookmark-border",
-    "review": "rate-review",
-    "arrow.right" : "arrow-forward",
-    search: "search",
-    "location.pin": "location-pin",
-    "calendar.month": "calendar-month",
-    euro: "euro",
-    "local.fire.department": "local-fire-department",
+  // See MaterialIcons here: https://icons.expo.fyi
+  // See SF Symbols in the SF Symbols app on Mac.
+  "house.fill": "home",
+  "paperplane.fill": "send",
+  "chevron.left.forwardslash.chevron.right": "code",
+  "chevron.right": "chevron-right",
+  "notifications.none": "notifications-none",
+  "calendar.today": "calendar-today",
+  people: "emoji-people",
+  location: "location-pin",
+  bookmark: "bookmark-border",
+  review: "rate-review",
+  "arrow.right": "arrow-forward",
+  search: "search",
+  "location.pin": "location-pin",
+  "calendar.month": "calendar-month",
+  euro: "euro",
+  "local.fire.department": "local-fire-department",
+  "sports.soccer": "sports-soccer",
+  movie: "movie",
+  fastfood: "fastfood",
+  "music.note": "music-note",
+  "tehater.comedy": "theater-comedy",
 } as Partial<
-    Record<
-        import("expo-symbols").SymbolViewProps["name"],
-        React.ComponentProps<typeof MaterialIcons>["name"]
-    >
+  Record<
+    import("expo-symbols").SymbolViewProps["name"],
+    React.ComponentProps<typeof MaterialIcons>["name"]
+  >
 >;
 
 export type IconSymbolName = keyof typeof MAPPING;
@@ -40,23 +45,23 @@ export type IconSymbolName = keyof typeof MAPPING;
  * Icon `name`s are based on SFSymbols and require manual mapping to MaterialIcons.
  */
 export function IconSymbol({
-                               name,
-                               size = 24,
-                               color,
-                               style,
-                           }: {
-    name: IconSymbolName;
-    size?: number;
-    color: string | OpaqueColorValue;
-    style?: StyleProp<ViewStyle>;
-    weight?: SymbolWeight;
+  name,
+  size = 24,
+  color,
+  style,
+}: {
+  name: IconSymbolName;
+  size?: number;
+  color: string | OpaqueColorValue;
+  style?: StyleProp<ViewStyle>;
+  weight?: SymbolWeight;
 }) {
-    return (
-        <MaterialIcons
-            color={color}
-            size={size}
-            name={MAPPING[name]}
-            style={style}
-        />
-    );
+  return (
+    <MaterialIcons
+      color={color}
+      size={size}
+      name={MAPPING[name]}
+      style={style}
+    />
+  );
 }
